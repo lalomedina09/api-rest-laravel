140003b67607bf9587323c204626761c15d868a3
Eulalio Medina Barragan <eulalio.09@icloud.com>
2022-05-06 23:58:49 -0500

    modelo y migracion de tabla articulos

--- app/Models/Articulos.php
+++ app/Models/Articulos.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Articulos extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id', 'description', 'precio', 'stock'];
 }
--- database/migrations/2022_05_07_045217_create_articulos_table.php
+++ database/migrations/2022_05_07_045217_create_articulos_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('articulos', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->float('price');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
